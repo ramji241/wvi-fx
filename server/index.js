9cff902d66f002f7f7f920b9c3001a50fde6ac11
Ashwin Ramji <aar241@gmail.com>
2023-10-18 17:06:44 -0700

    made a bunch of changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const path = require('path')
 const express = require('express')
-const port = process.env.port || 3001
 const app = express()
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
+const connectDB = require('./config/database')
+const port = process.env.port || 3001
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+require('dotenv').config({path: './config/.env'})
+connectDB()
+
+app.use(express.static(path.resolve(__dirname, '../client/public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/api', (req, res) => res.json({message: 'Hello from server!'}))
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/build', 'index.html')))
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/public', 'index.html')))
+
 app.listen(port, () => console.log(`Server listening on ${port}...`))
