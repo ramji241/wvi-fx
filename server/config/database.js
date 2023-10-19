53d988d4ad12d1e377a38dbdeffc5bff3400dd13
Ashwin Ramji <aar241@gmail.com>
2023-10-18 17:15:43 -0700

    changed name of database in mongoose

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {dbName: "fnc-liturgy"})
+    const conn = await mongoose.connect(process.env.DB_STRING, {dbName: "wvigtfx"})
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (err) {
     console.error(err)
